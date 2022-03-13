0ad5044e9d67be0bd217ab3ec8b640828148f89e
Shadrack Limbua <shadrack.limbua1@nn.cz>
2022-03-13 12:46:02 +0100

    Added helper node list and able to add elements that are not all ordered according to occurence

--- huffman.py
+++ huffman.py
@@ -4,7 +4,7 @@ import sys
 class Node:
     def __init__(self, data):
         self.data = data
-        self.freq = 1
+        self.freq = 0
 
 #helper TreeNode class for construction of the tree
 class TreeNode:
@@ -38,9 +38,48 @@ class Tree:
     def get_root(self):
         return self.root
 
+#Helper class to store the nodes in a list thats following an order like prio queue
+class NodeList:
+    def __init__(self):
+        self.arr = list()
+    
+    def size(self):
+        return len(self.arr)
+
+    def add(self, node):
+        if self.size() == 0:
+            self.arr.append(node)
+        else:
+            last_node = self.arr[-1]
+            if last_node.freq < node.freq:
+                self.arr.append(node)
+            else:
+                self.arr.insert(-1, node)
+
+    #helper method to show me the elements in the nodes list   
+    def __repr__(self):
+        pr_str = ''
+        for idx, node in enumerate(self.arr):
+            pr_str += '---node at index {}---\n'.format(idx)
+            pr_str += node.data + '---'+ str(node.freq) + '\n'
+            pr_str += '---------------------\n'
+        return pr_str
+
+
 
 def huffman_encoding(data):
+    if data is None:
+        return None
+    data_cased = data.lower()
+    data_list = NodeList()
+    for chr in set(data_cased):
+        data_node = Node(chr)
+        data_node.freq = data.count(chr)
+        data_list.add(data_node)
+        
 
+    print(data_list)
+    return data_list
     pass
 
 def huffman_decoding(data, tree):
@@ -62,4 +101,4 @@ if __name__ == "__main__":
     print ("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
     print ("The content of the encoded data is: {}\n".format(decoded_data))
 
-huffman_encoding("AAAAAAABBBCCCCCCCDDEEEEEE")
+
