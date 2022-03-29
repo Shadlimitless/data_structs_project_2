7024658d2899d63a4a9025cf6fe63fd5d8cc439e
Shadrack Limbua <shadrack.limbua1@nn.cz>
2022-03-29 09:28:20 +0200

    Working LRU cache

--- lru.py
+++ lru.py
@@ -9,47 +9,65 @@ class LinkedListNode:
 class LRU_Cache(object):
 
     def __init__(self):
-        # Initialize class variables
+        # List that will be used as the hash map
         self.cache = [None for _ in range(5)]
         # List used as a queue to figure out the least used element in the cache
-        self.checker = []
-        self.size = 0
+        self.queue = []
+        self.num_entries = 0
         self.capacity = 5
-        # Use dictionary to store the key, value pairs...and then use a list thats using FIFO to track oldest element
         pass
 
     def get(self, key):
         # Retrieve item from provided key. Return -1 if nonexistent. 
-        return self.cache[key]
+        hash_index = self.get_hash_code(key)
+        head = self.cache[hash_index]
+        while head is not None:
+            if head.key == key:
+                self.queue.pop(0)
+                return head.value
+            head = head.next
+        return -1
         pass
 
     def set(self, key, value):
         # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item. 
         hash_index = self.get_hash_code(key)
-        self.size += 1
+        self.num_entries += 1
         # Find the head element at the index in case there is already a linkedlist there with key,value pairs
+        if self.num_entries > self.capacity:
+            least_used = self.queue.pop(0)
+            self.remove_least_used(least_used)
+        # Add new key to queue for tracking of least used element
+        self.queue.append(key)
         head = self.cache[hash_index]
         while head is not None:
             if head.key == key:
                 head.key = value
                 return
             head = head.next
+        # If we dont find the key above, we either have a new index or a collision happening, hence need to use a linkedlist to solve it
         new_node = LinkedListNode(key, value)
         head = self.cache[hash_index]
         new_node.next = head
-        self.cache[hash_index] = head
+        self.cache[hash_index] = new_node
         pass
 
-    def rehash(self):
+    def remove_least_used(self, key):
+        print(key)
+        hash_index = self.get_hash_code(key)
+        head = self.cache[hash_index]
+        while head is not None:
+            if head.key == key:
+                new_head = head.next
+                self.cache[hash_index] = new_head
+                self.num_entries -= 1
+                return
 
         pass
 
+    # Simple hash function for generating cache index
     def get_hash_code(self, key):
         return key % self.capacity
-
-    # Helper method for printing out the LRU cache for debugging purposes
-    def __repr__(self):
-        pass
 
 our_cache = LRU_Cache()
 
@@ -57,16 +75,14 @@ our_cache.set(1, 1)
 our_cache.set(2, 2)
 our_cache.set(3, 3)
 our_cache.set(4, 4)
+
+
+print(our_cache.get(1))       # returns 1
+print(our_cache.get(2))       # returns 2
+print(our_cache.get(9))      # returns -1 because 9 is not present in the cache
+
 our_cache.set(5, 5)
 our_cache.set(6, 6)
 
-
-# our_cache.get(1)       # returns 1
-# our_cache.get(2)       # returns 2
-# our_cache.get(9)      # returns -1 because 9 is not present in the cache
-
-# our_cache.set(5, 5) 
-# our_cache.set(6, 6)
-
-# our_cache.get(3)   
+print(our_cache.get(3))   
 
